183f7f37aa2e361fc20c35149b3255124a57fa4a
Rob Hirschfeld <rob_hirschfeld@dell.com>
2014-03-27 14:57:33 -0500

    pep8 compliance
    Change-Id: If860042276a912525618a760e96922c5001770df

--- t-container/tcup.py
+++ t-container/tcup.py
@@ -17,50 +17,57 @@
 
 # This file creates and runs Tempest in a Docker Container
 import os
-import subprocess
 import commands
-import sys
 import re
 
 if __name__ == "__main__":
 
-    # before we start, Confirm that they've sourced their openrc credentials already
-    for e in {'OS_PASSWORD','OS_USERNAME','OS_AUTH_URL'}:
-        if not os.environ.get(e):
-            raise Exception(e + 'Env Variable Missing: You may need to "source openrc.sh" first.')
+    # Confirm you've sourced their openrc credentials already
+    for e in {'OS_PASSWORD', 'OS_USERNAME', 'OS_AUTH_URL'}:
+        if not os.environ.get(e):   
+            e + 'Env Variable Missing: You may need to "source openrc.sh".'
+            raise Exception(exp)
 
     # build the container
-    print "Downloading & Building T-CUP Image...(the first run may take a long time)"
+    print "Downloading & Building T-CUP Image...(may take a long time)"
     docker_file_source = "t-container"
-    print "\texecuting `docker build " + docker_file_source+"`"
+    print "\texecuting `docker build " + docker_file_source + "`"
     build_output = commands.getoutput("docker build " + docker_file_source)
-    image = re.search("Successfully built ([0-9a-f]{12})", build_output).group(1)
-    print "\nT-CUP Built Docker Image ID: "+image
 
+    # provide friendly output progress message
+    search_for = "Successfully built ([0-9a-f]{12})"
+    image = re.search(search_for, build_output).group(1)
+    print "\nT-CUP Built Docker Image ID: " + image
 
     # collect environment variables to pass, we don't want all of them
     e_vars = dict(os.environ)
-    for e in {"LS_COLORS", "HOME", "PATH", "PWD", "OLDPWD", "LESSCLOSE", "SSH_CONNECTION"}:
-        try: e_vars.pop(e, None)
-        except:  pass
+    for e in {"LS_COLORS", "HOME", "PATH", "PWD", "OLDPWD", "LESSCLOSE", \
+              "SSH_CONNECTION"}:
+        try: 
+            e_vars.pop(e, None)
+        except: 
+            pass
 
     # test specific configuration
-    if not os.environ.get('TEST_ID'): e_vars["test_id"] = "1000"
+    if not os.environ.get('TEST_ID'): 
+        e_vars["test_id"] = "1000"  # TODO: generated good value!
 
-    if not os.environ.get('API_SERVER_ADDRESS'): e_vars["api_addr"] = "http://refstack.org/result"
+    if not os.environ.get('API_SERVER_ADDRESS'): 
+        e_vars["api_addr"] = "http://refstack.org/result"
 
     # create the docker run command line
     docker_run = "docker run -d -i"
-    for e in e_vars:
-      docker_run += ' -e "'+e+'='+e_vars[e]+'"'
-    docker_run += ' -t '+image
+    for e in e_vars: docker_run += ' -e "' + e + '=' + e_vars[e] + '"'
+    docker_run += ' -t '+ image
     debug = 1
-    if debug:
-        docker_run += " /bin/bash"
-    else:
-        docker_run += " cd refstack; python refstack/tools/execute_test.py --callback ${api_addr} ${test_id}"
-    print "\texecuting: '"+docker_run+"'"
+    if debug: 
+            docker_run += " /bin/bash"
+    else: 
+        docker_run += " cd refstack; python refstack/tools/execute_test.py"
+        docker_run += " --callback ${api_addr} ${test_id}"
+    print "\texecuting: '" + docker_run + "'"
     
     # start the container and advise the user about how to attach
     docker_output = commands.getoutput(docker_run)
-    print "\nYou can monitor the T-CUP results using (hint: you may have to press [enter])\n\t'sudo docker attach "+docker_output[0:12]+"'"
+    print "\nYou can monitor the T-CUP results using (hint: press [enter])"
+    print "\n\t'sudo docker attach " + docker_output[0:12] + "'"
